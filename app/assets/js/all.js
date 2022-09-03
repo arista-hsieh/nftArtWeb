1ca46f68224436750c447b2e7ef6ae36054d1fe1
Arista <arista.hsieh@gmail.com>
2022-09-03 11:32:07 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,44 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry(elem, {
+  // options
+  itemSelector: '.grid-item',
+  gutter: 24,
+  horizontalOrder: true,
+  fitWidth: true,
+});
+
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress(function () {
+  $grid.masonry('layout');
 });
